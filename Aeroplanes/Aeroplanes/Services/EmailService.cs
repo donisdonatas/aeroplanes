36314328f9d78a55c46b5fc5ad58c169b8509548
donisdonatas <donisdonatas@gmail.com>
2022-12-10 10:26:35 +0200

    EmailServises made with try/catch block in case if any error occure.

--- Aeroplanes/Aeroplanes/Services/EmailService.cs
+++ Aeroplanes/Aeroplanes/Services/EmailService.cs
@@ -8,30 +8,39 @@ namespace Aeroplanes.Servises
     {
         public void SendEmail(string reportContext, string recipientsEmail)
         {
-            var fromAddress = new MailAddress("donisdonatas@gmail.com", "Donatas");
-            var toAddress = new MailAddress(recipientsEmail, "Airport director");
-            const string fromPassword = "oqdtnnytijhqtowt";
-            const string subject = "Arriving airplanes report";
 
-            var smtp = new SmtpClient
+            try
             {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
-                Timeout = 2000
-            };
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = reportContext,
-                IsBodyHtml = true
-            })
-            {
-                smtp.Send(message);
+                var fromAddress = new MailAddress("donisdonatas@gmail.com", "Donatas");
+                var toAddress = new MailAddress(recipientsEmail, "Airport director");
+                const string fromPassword = "oqdtnnytijhqtowt";
+                const string subject = "Arriving airplanes report";
+
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
+                    Timeout = 2000
+                };
+                using (var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = reportContext,
+                    IsBodyHtml = true
+                })
+                {
+                    smtp.Send(message);
+                }
+                Console.WriteLine("The eMail was sent out.");
             }
-            Console.WriteLine("The eMail was sent out.");
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error ocurred trying send email: {ex}");
+                Console.WriteLine("Please check parameters and try again.");
+            }
         }
     }
 }
